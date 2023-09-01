285e926f74b2329891e62a8603f5ed6c944df74e
Dayu Huang <964552260@qq.com>
2023-09-01 16:09:36 +1000

    DO 100%

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -6,15 +6,24 @@ public class PrintAndHide : MonoBehaviour
 {
     private int i = 3;
     public Renderer rend;
+    private int randomNum;
     void Start()
     {
-        i++;
+        randomNum = Random.Range(150, 251);
     }
 
     // Update is called once per frame
     void Update()
     {
         i++;
-        Debug.Log(gameObject.name + ":" + i.ToString());
+        Debug.Log(gameObject.name + ":" + i);
+        if (gameObject.tag.Equals("Red") && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+        else if (gameObject.tag.Equals("Blue") && i == randomNum)
+        {
+            GetComponent<Renderer>().enabled = false;
+        }
     }
 }
